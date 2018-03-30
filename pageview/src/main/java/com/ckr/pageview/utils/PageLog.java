892df7bcf70965aaf6f831a9efe476bdad4d1cbd
ckrgithub <13560177212@163.com>
2018-03-30 15:54:37 +0800

    1.BuildConfigField定义一个字段来标记log的debug模式

--- pageview/src/main/java/com/ckr/pageview/utils/PageLog.java
+++ pageview/src/main/java/com/ckr/pageview/utils/PageLog.java
@@ -2,13 +2,15 @@ package com.ckr.pageview.utils;
 
 import android.util.Log;
 
+import com.ckr.pageview.BuildConfig;
+
 /**
  * Created by PC大佬 on 2018/1/26.
  */
 
 public class PageLog {
 	private static final String TAG = "PageLog";
-	private static boolean isDebug = true;
+	private static boolean isDebug = false;
 
 	public static void debug() {
 		PageLog.isDebug = true;
@@ -19,34 +21,37 @@ public class PageLog {
 	}
 
 	public static void Logd(String tag, String msg) {
-		if (isDebug) {
+		if (BuildConfig.isDebug || isDebug) {
 			Log.d(TAG, tag + "--->" + msg);
 		}
 	}
+
 	public static void Logi(String msg) {
 		Logi("", msg);
 	}
 
 	public static void Logi(String tag, String msg) {
-		if (isDebug) {
+		if (BuildConfig.isDebug || isDebug) {
 			Log.i(TAG, tag + "--->" + msg);
 		}
 	}
+
 	public static void Logw(String msg) {
 		Logw("", msg);
 	}
 
 	public static void Logw(String tag, String msg) {
-		if (isDebug) {
+		if (BuildConfig.isDebug || isDebug) {
 			Log.w(TAG, tag + "--->" + msg);
 		}
 	}
+
 	public static void Loge(String msg) {
 		Loge("", msg);
 	}
 
 	public static void Loge(String tag, String msg) {
-		if (isDebug) {
+		if (BuildConfig.isDebug || isDebug) {
 			Log.e(TAG, tag + "--->" + msg);
 		}
 	}
