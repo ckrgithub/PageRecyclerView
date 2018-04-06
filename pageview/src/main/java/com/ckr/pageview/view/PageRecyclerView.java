fb50daf3605dadb4c83b89cfe4d1640d69f581ff
ckrgithub <13560177212@163.com>
2018-04-06 10:37:47 +0800

    1.添加水平翻页动画

--- pageview/src/main/java/com/ckr/pageview/view/PageRecyclerView.java
+++ pageview/src/main/java/com/ckr/pageview/view/PageRecyclerView.java
@@ -289,6 +289,17 @@ public class PageRecyclerView extends RecyclerView {
 					mOnPageChangeListener.onPageSelected(mCurrentPage);
 				}
 			}
+			if (mPageTransformer != null) {
+				int scrollX = getScrollX();
+				int childCount = getChildCount();
+				for (int i = 0; i < childCount; i++) {
+					View child = getChildAt(i);
+					float transformPos = (child.getLeft() - scrollX) / (float) mScrollWidth;
+					Logd(TAG, "onScrolled: transformPos:" + transformPos + ",left:" + child.getLeft()
+							+ ",mScrollWidth:" + mScrollWidth + ",childCount:" + childCount + ",i:" + i);
+					mPageTransformer.transformPage(child, transformPos);
+				}
+			}
 		} else {
 			mScrollOffset += dy;
 			if (mScrollState == SCROLL_STATE_DRAGGING) {
@@ -319,17 +330,6 @@ public class PageRecyclerView extends RecyclerView {
 				if (mLastPage - mCurrentPage != 0) {
 					mOnPageChangeListener.onPageSelected(mCurrentPage);
 				}
-			}
-		}
-		if (mPageTransformer != null) {
-			int scrollX = getScrollX();
-			int childCount = getChildCount();
-			for (int i = 0; i < childCount; i++) {
-				View child = getChildAt(i);
-				float transformPos = (child.getLeft() - scrollX) / (float) mScrollWidth;
-				Logd(TAG, "onScrolled: transformPos:" + transformPos + ",left:" + child.getLeft()
-						+ ",mScrollWidth:" + mScrollWidth + ",childCount:" + childCount + ",i:" + i);
-				mPageTransformer.transformPage(child, transformPos);
 			}
 		}
 		Logd(TAG, "onScrolled,mCurrentPage:" + mCurrentPage);
