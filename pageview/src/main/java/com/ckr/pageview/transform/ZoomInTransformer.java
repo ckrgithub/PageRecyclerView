d5cba7396b475b9fd31afb27b5cc3f9958f01035
ckrgithub <13560177212@163.com>
2018-04-08 00:07:11 +0800

    1.实现竖直方向的 ZoomInTransformer

--- pageview/src/main/java/com/ckr/pageview/transform/ZoomInTransformer.java
+++ pageview/src/main/java/com/ckr/pageview/transform/ZoomInTransformer.java
@@ -18,16 +18,27 @@ package com.ckr.pageview.transform;
 
 import android.view.View;
 
+import com.ckr.pageview.adapter.OnPageDataListener;
+
 public class ZoomInTransformer extends BaseTransformer {
 
 	@Override
 	protected void onTransform(View view, float position, boolean forwardDirection, int mOrientation) {
 		final float scale = position < 0 ? position + 1f : Math.abs(1f - position);
-		view.setScaleX(scale);
-		view.setScaleY(scale);
-		view.setPivotX(view.getWidth() * 0.5f);
-		view.setPivotY(view.getHeight() * 0.5f);
-		view.setAlpha(position < -1f || position > 1f ? 0f : 1f - (scale - 1f));
+		if (mOrientation == OnPageDataListener.HORIZONTAL) {
+			view.setScaleX(scale);
+			view.setScaleY(scale);
+			view.setPivotX(view.getWidth() * 0.5f);
+			view.setPivotY(view.getHeight() * 0.5f);
+			view.setAlpha(position < -1f || position > 1f ? 0f : 1f - (scale - 1f));
+		} else {
+			view.setScaleX(scale);
+			view.setScaleY(scale);
+			view.setPivotX(view.getWidth() * 0.5f);
+			view.setPivotY(view.getHeight() * 0.5f);
+			view.setAlpha(position < -1f || position > 1f ? 0f : 1f - (scale - 1f));
+		}
+
 	}
 
 }
