84c1e4552237533509982065fc00bc205a79f113
ckrgithub <13560177212@163.com>
2018-04-07 23:49:22 +0800

    1.修复竖直方向的CubeOutTransformer

--- pageview/src/main/java/com/ckr/pageview/transform/CubeOutTransformer.java
+++ pageview/src/main/java/com/ckr/pageview/transform/CubeOutTransformer.java
@@ -3,6 +3,8 @@ package com.ckr.pageview.transform;
 import android.support.v4.view.ViewCompat;
 import android.view.View;
 
+import com.ckr.pageview.adapter.OnPageDataListener;
+
 /**
  * Created by PC大佬 on 2018/4/6.
  */
@@ -12,9 +14,15 @@ public class CubeOutTransformer extends BaseTransformer {
 
 	@Override
 	protected void onTransform(View view, float position, boolean forwardDirection, int mOrientation) {
-		ViewCompat.setPivotX(view, position < 0f ? view.getWidth() : 0f);
-		ViewCompat.setPivotY(view, view.getHeight() * 0.5f);
-		ViewCompat.setRotationY(view, 90f * position);
+		if (mOrientation == OnPageDataListener.HORIZONTAL) {
+			ViewCompat.setPivotX(view, position < 0f ? view.getWidth() : 0f);
+			ViewCompat.setPivotY(view, view.getHeight() * 0.5f);
+			ViewCompat.setRotationY(view, 90f * position);
+		} else {
+			ViewCompat.setPivotY(view, position < 0f ? view.getHeight() : 0f);
+			ViewCompat.setPivotX(view, view.getWidth() * 0.5f);
+			ViewCompat.setRotationX(view, -90f * position);
+		}
 	}
 
 }
