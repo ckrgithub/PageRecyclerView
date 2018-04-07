1f619ca6f33956bcff5da810798724e574844ebb
ckrgithub <13560177212@163.com>
2018-04-07 23:58:28 +0800

    1.实现竖直方向的ScaleInOutTransformer

--- pageview/src/main/java/com/ckr/pageview/transform/ScaleInOutTransformer.java
+++ pageview/src/main/java/com/ckr/pageview/transform/ScaleInOutTransformer.java
@@ -2,20 +2,34 @@ package com.ckr.pageview.transform;
 
 import android.view.View;
 
+import com.ckr.pageview.adapter.OnPageDataListener;
+
 public class ScaleInOutTransformer extends BaseTransformer {
 
 	@Override
 	protected void onTransform(View view, float position, boolean forwardDirection, int mOrientation) {
-		view.setPivotX(position < 0 ? 0 : view.getWidth());
-		view.setPivotY(view.getHeight() / 2f);
-		float scale = position < 0 ? 1f + position : 1f - position;
-		view.setScaleX(scale);
-		view.setScaleY(scale);
+		if (mOrientation == OnPageDataListener.HORIZONTAL) {
+			view.setPivotX(position < 0 ? 0 : view.getWidth());
+			view.setPivotY(view.getHeight() / 2f);
+			float scale = position < 0 ? 1f + position : 1f - position;
+			view.setScaleX(scale);
+			view.setScaleY(scale);
+		} else {
+			view.setPivotY(position < 0 ? 0 : view.getHeight());
+			view.setPivotX(view.getWidth() / 2f);
+			float scale = position < 0 ? 1f + position : 1f - position;
+			view.setScaleX(scale);
+			view.setScaleY(scale);
+		}
 	}
 
 	@Override
 	protected void onPreTransform(View view, float position, int mOrientation) {
 		super.onPreTransform(view, position, mOrientation);
-		view.setTranslationX(-view.getWidth() * position);
+		if (mOrientation == OnPageDataListener.HORIZONTAL) {
+			view.setTranslationX(-view.getWidth() * position);
+		} else {
+			view.setTranslationY(-view.getHeight() * position);
+		}
 	}
 }
