7abc9e066ec4e9aba0a8462b4db157e0ab6687d7
Ckr <ckr19931129@gmail.com>
2021-08-06 15:27:10 +0800

    add 新增BaseViewHolder，为了在androidX项目使用

---
+++ pageview/src/main/java/com/ckr/pageview/adapter/BaseViewHolder.java
@@ -0,0 +1,10 @@
+package com.ckr.pageview.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public abstract class BaseViewHolder extends RecyclerView.ViewHolder {
+    public BaseViewHolder(View itemView) {
+        super(itemView);
+    }
+}
